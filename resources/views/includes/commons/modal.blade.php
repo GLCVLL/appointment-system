ac512292ea846c504224485ff3eb796899efc9f9
l3on <l3onwebdev@gmail.com>
2024-01-07 20:57:23 +0100

    added modal layout

---
+++ resources/views/includes/commons/modal.blade.php
@@ -0,0 +1,20 @@
+<div class="app-modal">
+    <div class="app-modal-content">
+        <div class="app-modal-title">
+            Title
+            <button class="btn app-modal-close" data-close>
+                <i class="fas fa-close fa-2xl"></i>
+            </button>
+        </div>
+
+        <div class="app-modal-body">
+            Lorem...
+        </div>
+
+        <div class="app-modal-footer">
+            <button class="btn btn-outline-secondary me-2" data-close>Cancel</button>
+            <button class="btn btn-primary app-modal-submit" data-close>Ok</button>
+        </div>
+
+    </div>
+</div>
