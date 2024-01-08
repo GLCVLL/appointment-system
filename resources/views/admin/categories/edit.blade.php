f006fd307397d975a684f71c6aa3ccfaaadb00a9
GLCVLL <g.ioele96@outlook.it>
2024-01-08 12:19:41 +0100

    made index

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', ' - Edit Category')
+
+@section('content')
+    <section class="container-fluid py-4">
+
+        {{-- Header --}}
+        <header class="mb-3">
+
+            {{-- Title --}}
+            <div class="d-flex align-items-center gap-3">
+
+                {{-- Back --}}
+                <a href="{{ route('admin.categories.index') }}" class="btn btn-light">
+                    <i class="fa-solid fa-chevron-left fa-xl"></i>
+                </a>
+
+                {{-- Title --}}
+                <h2 class="mb-0">Edit Category</h2>
+
+            </div>
+
+        </header>
+
+        <hr class="mb-4">
+
+        {{-- Form --}}
+        @include('includes.category.form')
+
+    </section>
+@endsection
