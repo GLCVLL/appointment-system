d3fa2e2779b6652171521a494b3bf613192d9af8
GLCVLL <g.ioele96@outlook.it>
2024-01-08 13:30:58 +0100

    added data to services

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $services_data = [
+            [
+                'category_id' => 1,
+                'name' => 'Taglio e styling',
+                'duration' => '01:00:00',
+                'is_available' => true,
+            ],
+            [
+                'category_id' => 2,
+                'name' => 'Colorazione capelli',
+                'duration' => '02:00:00',
+                'is_available' => true,
+            ],
+        ];
+
+        foreach ($services_data as $data) {
+            $service = new Service();
+            $service->fill($data);
+            $service->save();
+        }
     }
 }
