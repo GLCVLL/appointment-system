08948361d0a96f775a5b20fc21559fb7a9245b06
GLCVLL <g.ioele96@outlook.it>
2024-01-08 17:37:09 +0100

    fixed seeder

--- database/seeders/AppointmentSeeder.php
+++ database/seeders/AppointmentSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Appointment;
+use App\Models\Service;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class AppointmentSeeder extends Seeder
 {
@@ -14,6 +16,8 @@ class AppointmentSeeder extends Seeder
      */
     public function run(Generator $faker): void
     {
+        $services = Service::pluck('id')->toArray();
+
         for ($i = 0; $i <= 2; $i++) {
             $appointment = new Appointment();
             $appointment->user_id = 1;
@@ -21,6 +25,16 @@ class AppointmentSeeder extends Seeder
             $appointment->start_time = $faker->time("H:i:s");
             $appointment->end_time = $faker->time("H:i:s");
             $appointment->save();
+
+            $selectedServices = [];
+
+            foreach ($services as $service) {
+                if (rand(0, 1)) $selectedServices[] = $service;
+            }
+
+            if (!count($selectedServices)) Arr::random($services);
+
+            $appointment->services()->attach($selectedServices);
         };
     }
 }
