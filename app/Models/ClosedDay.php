ff0b560e4879ba3738eddb69d20b567dba06a49e
l3on <l3onwebdev@gmail.com>
2024-01-08 13:02:37 +0100

    added date utils

--- app/Models/ClosedDay.php
+++ app/Models/ClosedDay.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,15 @@ class ClosedDay extends Model
     use HasFactory;
 
     protected $fillable = ['date'];
+
+
+    //*** UTILITIES ***//
+    /**
+     * Format a date field
+     */
+    public function getDate($date_field, $format = 'd/m/y H:i')
+    {
+        return Carbon::create($this->$date_field)
+            ->format($format);
+    }
 }
