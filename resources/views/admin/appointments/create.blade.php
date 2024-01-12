a9aa7d7b210f162588cabde0ec00917e7bb01a3a
l3on <l3onwebdev@gmail.com>
2024-01-12 12:00:55 +0100

    added start time and end time dynamic logic

--- resources/views/admin/appointments/create.blade.php
+++ resources/views/admin/appointments/create.blade.php
@@ -33,5 +33,90 @@
 
 
 @section('scripts')
+
     @vite(['resources/js/validations/appointment-form'])
+
+    <script>
+        // Get DOM Elems
+        const dateInput = document.getElementById('date');
+        const startTimeInput = document.getElementById('start_time');
+        const endTimeInput = document.getElementById('end_time');
+
+        // Get Generic Data
+        const dayOfWeeks = @json(config('data.day_of_week'));
+        const openingHours = @json($openingHours);
+
+
+        // Check if disabled
+        if (!dateInput.value) {
+            startTimeInput.disabled = true;
+            startTimeInput.selectedIndex = 0;
+            endTimeInput.disabled = true;
+            endTimeInput.selectedIndex = 0;
+        }
+
+        dateInput.addEventListener('change', () => {
+
+            // Check if disabled
+            if (!dateInput.value) {
+                startTimeInput.disabled = true;
+                endTimeInput.disabled = true;
+            } else {
+
+
+                const dayOfWeek = dayOfWeeks[new Date(dateInput.value).getDay()];
+                const currentOpeningHour = openingHours.find(({
+                    day
+                }) => day === dayOfWeek);
+
+
+                if (currentOpeningHour) {
+
+                    let timeArray = createTimeArray(currentOpeningHour.opening_time, currentOpeningHour
+                        .closing_time, 30);
+
+                    let options = '<option value = "" > -- -- </option>';
+                    timeArray.forEach(time => {
+                        options += `<option value="${time.value}">${time.text}</option>`;
+                    });
+
+                    startTimeInput.innerHTML = options;
+                    startTimeInput.disabled = false;
+                    endTimeInput.innerHTML = options;
+                    endTimeInput.disabled = false;
+
+                } else {
+                    startTimeInput.selectedIndex = 0;
+                    startTimeInput.disabled = true;
+                    endTimeInput.selectedIndex = 0;
+                    endTimeInput.disabled = true;
+                }
+
+            }
+        });
+
+
+        function createTimeArray(openingTime, closingTime, intervalMinutes) {
+
+            let openingTimeArray = openingTime.split(':');
+            let closingTimeArray = closingTime.split(':');
+            let timeArray = [];
+            let currentTime = new Date();
+            currentTime.setHours(openingTimeArray[0], openingTimeArray[1], openingTimeArray[2]);
+
+            let endTime = new Date();
+            endTime.setHours(closingTimeArray[0], closingTimeArray[1], closingTimeArray[2]);
+
+            while (currentTime <= endTime) {
+                timeArray.push({
+                    value: currentTime.toTimeString().substring(0, 5), // "HH:MM"
+                    text: currentTime.toTimeString().substring(0, 5) // "HH:MM"
+                });
+
+                currentTime.setMinutes(currentTime.getMinutes() + intervalMinutes);
+            }
+
+            return timeArray;
+        }
+    </script>
 @endsection
