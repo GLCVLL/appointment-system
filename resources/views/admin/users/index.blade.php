69635dc98a6059918a4555cb878c0ff1e2381b30
l3on <l3onwebdev@gmail.com>
2024-01-17 16:10:17 +0100

    fixed index style and modal

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,67 +3,69 @@
 @section('title', ' - Users')
 
 @section('content')
-    <section class="container-fluid py-4">
+    <section class="p-4">
 
-        {{-- Header --}}
-        <header class="d-flex justify-content-between align-items-center mb-3">
-            {{-- Title --}}
-            <h2 class="mb-0">Users</h2>
-            {{-- Add User --}}
-            <a href="{{ route('admin.users.create') }}" class="btn btn-success btn-circle">
-                <i class="fas fa-plus fa-lg"></i>
-            </a>
-        </header>
+        <div class="container-fluid">
 
-        <hr class="mb-4">
+            {{-- Header --}}
+            <header class="d-flex justify-content-between align-items-center mb-4">
+                {{-- Title --}}
+                <h2 class="mb-0">Users</h2>
+                {{-- Add User --}}
+                <a href="{{ route('admin.users.create') }}" class="btn btn-success btn-circle">
+                    <i class="fas fa-plus fa-lg"></i>
+                </a>
+            </header>
 
-        {{-- List --}}
-        <div class="table-responsive px-4">
-            <table class="table table-striped align-middle">
-                <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Role</th>
-                        <th scope="col" class="text-center">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($users as $user)
+            {{-- List --}}
+            <div class="table-responsive card p-3">
+                <table class="table table-hover align-middle">
+                    <thead>
                         <tr>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>{{ ucfirst($user->role) }}</td>
-                            <td class="text-center">
-                                {{-- Actions --}}
-                                <div class="d-flex justify-content-center gap-2">
-                                    {{-- Edit --}}
-                                    <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-warning">
-                                        <i class="fas fa-pencil-alt"></i>
-                                    </a>
-                                    {{-- Delete --}}
-                                    <form action="{{ route('admin.users.destroy', $user) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-danger" onclick="return confirm('Are you sure?')">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Role</th>
+                            <th scope="col"></th>
                         </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4" class="text-center p-3">No Users Found</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @forelse ($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ ucfirst($user->role) }}</td>
+                                <td>
+                                    {{-- Actions --}}
+                                    <div class="d-flex justify-content-end gap-2">
+                                        {{-- Edit --}}
+                                        <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-warning">
+                                            <i class="fas fa-pencil-alt"></i>
+                                        </a>
+                                        {{-- Delete --}}
+                                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST"
+                                            class="delete-form" data-modal-name="{{ $user->name }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-danger">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="4" class="text-center p-3">No Users Found</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+
         </div>
-
     </section>
 @endsection
 
 @section('scripts')
-    @vite(['resources/js/commons/modal-delete'])
+    @vite(['resources/js/commons/modal-delete.js'])
 @endsection
