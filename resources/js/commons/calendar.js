b6d500e1d47d7cace8bf50875036daa37cf69a84
l3on <l3onwebdev@gmail.com>
2024-01-18 18:46:18 +0100

    centralized logic

---
+++ resources/js/commons/calendar.js
@@ -0,0 +1,45 @@
+import { Calendar } from 'fullcalendar'
+
+// Get Elem
+const calendarEl = document.getElementById('calendar');
+
+// Check if exist
+if (calendarEl && calendarEl.dataset.events) {
+
+    // On Content loaded
+    document.addEventListener('DOMContentLoaded', function () {
+
+        // parse events
+        let events = JSON.parse(calendarEl.dataset.events);
+
+        // Calendar config
+        let calendar = new Calendar(calendarEl, {
+            headerToolbar: {
+                left: "prev,today,next",
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+            },
+            slotLabelFormat: {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+            },
+            eventTimeFormat: {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+            },
+            initialView: 'timeGridWeek',
+            slotMinTime: '8:00:00',
+            slotMaxTime: '19:00:00',
+            allDaySlot: false,
+            eventColor: '#6571ff88',
+            events,
+            dateClick: function (info) {
+                console.log(info);
+            },
+        });
+
+        calendar.render();
+    });
+}
--- resources/views/admin/appointments/calendar.blade.php
+++ resources/views/admin/appointments/calendar.blade.php
@@ -20,46 +20,12 @@
             </header>
 
             {{-- Calendar --}}
-            <div id="calendar"></div>
+            <div id="calendar" data-events='@json($events)'></div>
 
         </div>
     </section>
 @endsection
 
 @section('scripts')
-    @vite(['resources/js/commons/modal-delete.js'])
-
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            let calendarEl = document.getElementById('calendar');
-            let calendar = new FullCalendar.Calendar(calendarEl, {
-                headerToolbar: {
-                    left: "prev,today,next",
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-                },
-                slotLabelFormat: {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: false
-                },
-                eventTimeFormat: {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: false
-                },
-                initialView: 'timeGridWeek',
-                slotMinTime: '8:00:00',
-                slotMaxTime: '19:00:00',
-                allDaySlot: false,
-                eventColor: '#6571ff88',
-                events: @json($events),
-                dateClick: function(info) {
-                    console.log(info);
-                },
-            });
-            calendar.render();
-        });
-    </script>
+    @vite(['resources/js/commons/calendar.js'])
 @endsection
