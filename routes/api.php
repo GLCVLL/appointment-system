addd2b6aa79b270a7762c6c8664bafe7f6083f9b
GLCVLL <g.ioele96@outlook.it>
2024-01-26 12:20:10 +0100

    added Auth, booking-hours & store appointments routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+// Authentication Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::delete('/logout', [AuthController::class, 'logout']);
+
+// Appoimtments Routes
+Route::post('/appointments', [AppointmentController::class, 'store'])->middleware('auth:sanctum');
+
+// Opening-Hours Routes
+Route::get('/booking-hours', [AppointmentController::class, 'getBookingHours']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
