d8e36fa4d8a48d208765df1dc6af94adfd553ef9
l3on <l3onwebdev@gmail.com>
2024-01-29 13:08:15 +0100

    fixed responsive view on tables

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -24,7 +24,7 @@
                         <tr>
                             <th scope="col">Name</th>
                             <th scope="col" class="text-center">Duration</th>
-                            <th scope="col" class="text-center">Availability</th>
+                            <th scope="col" class="text-center d-none d-sm-table-cell">Availability</th>
                             <th scope="col"></th>
                         </tr>
                     </thead>
@@ -33,7 +33,8 @@
                             <tr>
                                 <td>{{ $service->name }}</td>
                                 <td class="text-center">{{ $service->getDate('duration', 'H:i') }}</td>
-                                <td class="text-center">{{ $service->is_available ? 'Yes' : 'No' }}</td>
+                                <td class="text-center d-none d-sm-table-cell">{{ $service->is_available ? 'Yes' : 'No' }}
+                                </td>
                                 <td class="text-end">
                                     {{-- Actions --}}
                                     <div class="d-flex justify-content-center gap-2">
