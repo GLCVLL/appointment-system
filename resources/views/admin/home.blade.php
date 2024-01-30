32b7da682bf58fffe83ee5f45f41598f28a1f2f0
l3on <l3onwebdev@gmail.com>
2024-01-30 16:44:16 +0100

    added base layout

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,26 +3,53 @@
 @section('content')
     <section class="py-4 px-md-4">
         <div class="container-fluid">
-            <h2 class="mb-4">
-                {{ __('Dashboard') }}
-            </h2>
-            <div class="row justify-content-center">
+
+            <header class="d-flex justify-content-between align-items-center mb-4">
+
+                {{-- Title --}}
+                <h2 class="mb-0">
+                    {{ __('Dashboard') }}
+                </h2>
+
+                {{-- Filters --}}
+                <form class="row g-3 align-items-center">
+
+                    <div class="col-auto">
+                        <input type="date" id="date_min" class="form-control" name="date_min"
+                            value="{{ $filters['date_min'] }}">
+                    </div>
+
+                    <div class="col-auto">
+                        <input type="date" id="date_max" class="form-control" name="date_max"
+                            value="{{ $filters['date_max'] }}">
+                    </div>
+
+                    <div class="col-auto">
+                        <button class="btn btn-success"><i class="fas fa-rotate"></i></button>
+                    </div>
+
+                </form>
+
+            </header>
+
+            <div class="row">
                 <div class="col">
+
+                    {{-- New Clients Stats --}}
                     <div class="card p-2">
-                        <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
+                        <h3 class="text-center">New Clients</h3>
 
-                            {{ __('You are logged in!') }}
-                        </div>
+                        <canvas id="chart-users" data-values='@json($clients_data)'
+                            data-labels='@json($clients_labels)'></canvas>
+
                     </div>
                 </div>
             </div>
         </div>
     </section>
+@endsection
+
+@section('scripts')
+    @vite(['resources/js/charts/chart.js'])
 @endsection
