6874f41e7853f913b4d5dc6f35db52056838c2bb
l3on <l3onwebdev@gmail.com>
2024-01-30 16:50:41 +0100

    centralized chart logic

--- resources/js/charts/chart.js
+++ resources/js/charts/chart.js
@@ -1,27 +1,32 @@
 import Chart from 'chart.js/auto';
-import { formatDate } from '~resources/js/utils/';
+
+//*** FUNCTIONS ***//
+const initChart = (title, labels, data) => {
+    return new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels,
+            datasets: [{
+                label: title,
+                data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
 
 
+//*** DATA ***//
 // New Clients Chart
 const ctx = document.getElementById('chart-users');
 const clientLabels = JSON.parse(ctx.dataset.labels);
 const clientsData = JSON.parse(ctx.dataset.values);
 
-new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: clientLabels,
-        datasets: [{
-            label: '# of Clients',
-            data: clientsData,
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+initChart('# of Clients', clientLabels, clientsData);
