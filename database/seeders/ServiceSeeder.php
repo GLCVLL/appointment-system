72d5477b914ef9557bce53d6862c5ef5c3c1f2aa
l3on <l3onwebdev@gmail.com>
2024-01-30 17:20:35 +0100

    added price column to service resource

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -18,12 +18,14 @@ class ServiceSeeder extends Seeder
                 'category_id' => 1,
                 'name' => 'Taglio e styling',
                 'duration' => '01:00:00',
+                'price' => 15.00,
                 'is_available' => true,
             ],
             [
                 'category_id' => 2,
                 'name' => 'Colorazione capelli',
                 'duration' => '02:00:00',
+                'price' => 20.00,
                 'is_available' => true,
             ],
         ];
