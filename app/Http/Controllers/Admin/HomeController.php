20869a6f536b72d94a9a3c5f3a790c841e8e4d47
l3on <l3onwebdev@gmail.com>
2024-01-30 17:03:14 +0100

    minor fix

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -45,7 +45,7 @@ class HomeController extends Controller
 
             // Create chart data
             $clients_data[] = count($filtered_clients);
-            $clients_labels[] = $day->format('m-d');
+            $clients_labels[] = $day->format('m/d');
         }
 
 
