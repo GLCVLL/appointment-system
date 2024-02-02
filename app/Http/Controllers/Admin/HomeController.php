705dbb67503a3078864a06948c01beeecb4f6474
l3on <l3onwebdev@gmail.com>
2024-02-02 20:46:13 +0100

    added increment calculation and centralized logic

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -17,33 +17,81 @@ class HomeController extends Controller
         $filters = $request->all();
 
         // Validate
-        if (!isset($filters['date_min']) || !isset($filters['date_max']) || $filters['date_max'] <= $filters['date_min']) {
+        if (!isset($filters['date_min']) || !isset($filters['date_max']) || $filters['date_max'] < $filters['date_min']) {
             $filters['date_min'] = Carbon::now()->subWeek()->format('Y-m-d');
             $filters['date_max'] = Carbon::now()->format('Y-m-d');
         }
 
-        //*** DATA ***//
-        // Create Date Period
-        $period = new CarbonPeriod($filters['date_min'], $filters['date_max']);
 
-        // Get all data
+        //*** STATS ***//
+        // Get selected period stats
+        $stats = $this->calcStats($filters['date_min'], $filters['date_max']);
+
+        // Previous Date Period
+        $date_min = Carbon::createFromFormat('Y-m-d', $filters['date_min']);
+        $date_max = Carbon::createFromFormat('Y-m-d', $filters['date_max']);
+        $prev_period_start = $date_min->copy()->subDays($date_max->diffInDays($date_min));
+        $prev_period_end = $date_min->copy()->subDay();
+
+        // Get previous period stats
+        $stats_prev = $this->calcStats($prev_period_start, $prev_period_end);
+
+        // Create stats vars
+        $clients_chart = $stats['clients_chart'];
+        $clients_chart_prev = $stats_prev['clients_chart'];
+        $appointments_chart = $stats['appointments_chart'];
+        $appointments_chart_prev = $stats_prev['appointments_chart'];
+        $profits_chart = $stats['profits_chart'];
+        $profits_chart_prev = $stats_prev['profits_chart'];
+
+        // Calculate increments
+        $clients_chart['increment'] = $this->calcIncrement($clients_chart['tot'], $clients_chart_prev['tot']);
+        $appointments_chart['increment'] = $this->calcIncrement($appointments_chart['tot'], $appointments_chart_prev['tot']);
+        $profits_chart['increment'] = $this->calcIncrement($profits_chart['tot'], $profits_chart_prev['tot']);
+
+        return view('admin.home', compact('filters', 'clients_chart', 'appointments_chart', 'profits_chart'));
+    }
+
+
+    /**
+     * Calculate stats based on a date period
+     */
+    public function calcStats($date_min, $date_max)
+    {
+
+        //*** DATA ***//
+        $clients_chart = [
+            'data' => [],
+            'labels' => [],
+            'tot' => 0,
+            'increment' => 0,
+        ];
+        $appointments_chart = [
+            'data' => [],
+            'labels' => [],
+            'tot' => 0,
+            'increment' => 0,
+        ];
+        $profits_chart = [
+            'data' => [],
+            'labels' => [],
+            'tot' => 0,
+            'increment' => 0,
+        ];
+        $period = new CarbonPeriod($date_min, $date_max);
+
+        // Get resources
         $clients = User::where('role', 'user')
-            ->whereDate('created_at', '>=', $filters['date_min'])
-            ->whereDate('created_at', '<=', $filters['date_max'])
+            ->whereDate('created_at', '>=', $date_min)
+            ->whereDate('created_at', '<=', $date_max)
             ->orderBy('created_at')
             ->get();
 
         $appointments = Appointment::with('services')
-            ->whereDate('date', '>=', $filters['date_min'])
-            ->whereDate('date', '<=', $filters['date_max'])
+            ->whereDate('date', '>=', $date_min)
+            ->whereDate('date', '<=', $date_max)
             ->get();
 
-
-
-        //*** STATS ***//
-        $clients_chart = [];
-        $appointments_chart = [];
-        $profits_chart = [];
 
         foreach ($period as $day) {
 
@@ -95,6 +143,25 @@ class HomeController extends Controller
         $profits_chart['tot'] = array_sum($profits_chart['data']);
 
 
-        return view('admin.home', compact('filters', 'clients_chart', 'appointments_chart', 'profits_chart'));
+        return [
+            'clients_chart' => $clients_chart,
+            'appointments_chart' => $appointments_chart,
+            'profits_chart' => $profits_chart,
+        ];
+    }
+
+
+    /**
+     * Calculate stat increment in percentage
+     */
+    public function calcIncrement($sum, $prev_sum)
+    {
+        // Check division by 0
+        $divisor = $prev_sum === 0 ? 1 : $prev_sum;
+
+        // Calculate increment
+        $increment = ($sum - $prev_sum) / $divisor * 100;
+
+        return number_format($increment, 0);
     }
 }
