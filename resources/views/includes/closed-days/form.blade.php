14a6e080625713fb5a486ac087317d1adb35027a
vmazzarella <vmazzarella@extelos.com>
2025-11-23 18:31:59 +0100

    fix: update closed day validation to prevent duplicate entries and normalize date handling

--- resources/views/includes/closed-days/form.blade.php
+++ resources/views/includes/closed-days/form.blade.php
@@ -1,33 +1,34 @@
 @if ($closedDay->exists)
-    <form id="validation-form" class="card p-3" method="POST" action="{{ route('admin.closed-days.update', $closedDay) }}"
-        enctype="multipart/form-data" novalidate>
-        @method('PUT')
+<form id="validation-form" class="card p-3" method="POST" action="{{ route('admin.closed-days.update', $closedDay) }}"
+    enctype="multipart/form-data" novalidate>
+    @method('PUT')
     @else
-        <form id="validation-form" class="card p-3" method="POST" action="{{ route('admin.closed-days.store') }}"
-            enctype="multipart/form-data" novalidate>
-@endif
-@csrf
+    <form id="validation-form" class="card p-3" method="POST" action="{{ route('admin.closed-days.store') }}"
+        enctype="multipart/form-data" novalidate>
+        @endif
+        @csrf
 
-<div class="row">
+        <div class="row">
 
-    {{-- Date --}}
-    <div class="col-12 col-md-7 mb-4">
+            {{-- Date --}}
+            <div class="col-12 col-md-7 mb-4">
 
-        <label for="date" class="form-label fs-5">Date</label>
+                <label for="date" class="form-label fs-5">Date</label>
 
-        <input id="date" type="date" class="form-control @error('date') is-invalid @enderror"
-            value="{{ old('opening_time', $closedDay->date) }}" name="date">
-        @error('date')
-            <span class="invalid-feedback error-message" role="alert">{{ $message }}</span>
-        @enderror
-        <span id="date-error" class="invalid-feedback"></span>
-    </div>
+                <input id="date" type="date" class="form-control @error('date') is-invalid @enderror"
+                    value="{{ old('date', $closedDay->date) }}" name="date">
+                <small>Note: The year will be ignored. This date will be considered closed every year.</small>
+                @error('date')
+                <span class="invalid-feedback error-message" role="alert">{{ $message }}</span>
+                @enderror
+                <span id="date-error" class="invalid-feedback"></span>
+            </div>
 
 
-    {{-- # Submit --}}
-    <div class="col-12">
-        <button class="btn btn-primary">Confirm</button>
-    </div>
+            {{-- # Submit --}}
+            <div class="col-12">
+                <button class="btn btn-primary">Confirm</button>
+            </div>
 
-</div>
-</form>
+        </div>
+    </form>
