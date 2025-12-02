d9dadfd8573f0b94a590402d381a8f3de2e39b16
vmazzarella <vmazzarella@extelos.com>
2025-12-02 21:20:18 +0100

    hotfix: js file reference on manifest for build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,22 @@ export default defineConfig({
     plugins: [
         laravel({
             // edit the first value of the array input to point to our new sass files and folder.
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/js/app.js',
+                'resources/js/commons/modal.js',
+                'resources/js/commons/sidebar-toggler.js',
+                'resources/js/commons/modal-delete.js',
+                'resources/js/commons/modal-toggle-missed.js',
+                'resources/js/commons/calendar.js',
+                'resources/js/validations/user-form.js',
+                'resources/js/validations/services-form.js',
+                'resources/js/validations/opening-hours-form.js',
+                'resources/js/validations/closed-days-form.js',
+                'resources/js/validations/categories-form.js',
+                'resources/js/validations/appointment-form.js',
+                'resources/js/charts/chart.js',
+            ],
             refresh: true,
         }),
     ],
